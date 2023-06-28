4e53db2432d2da4e049262efa463f8921ef36b9f
ElsaSolen <97947759+ElsaSolen@users.noreply.github.com>
2023-06-28 14:23:27 +0100

    fix timer component decorator

--- app/timer/timer.component.ts
+++ app/timer/timer.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-tabs',
-  templateUrl: './tabs.component.html',
-  styleUrls: ['./tabs.component.scss'],
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.scss'],
 })
 export class TimerComponent implements OnInit {
   constructor() {}
